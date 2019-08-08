5ed77c30505adb232ac041b56f022a41830e0bd0
yhw-miracle <yhw_software@qq.com>
2019-08-08 20:24:41 +0800

    字典操作

--- object/jinse/demo.py
+++ object/jinse/demo.py
@@ -9,5 +9,13 @@ import time
 
 if __name__ == '__main__':
     # 将时间戳格式化
-    print(time.ctime(1565227826), type(time.ctime(1565227826)))
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(1565227826)))
+    # print(time.ctime(1565227826), type(time.ctime(1565227826)))
+    # print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(1565227826)))
+
+    # -----------------------------------------------------------------------
+    # 字典操作
+    d = {"1": {"2": 3}}
+    print(d.get("1").get("2"))
+
+    d = {"status_code": 500}
+    print("status_code" in d.keys())
