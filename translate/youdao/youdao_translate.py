cb9615f84ada828ea27d104a3d9957e8b46a1d5a
yhw-miracle <yhw_software@qq.com>
2019-08-10 22:24:35 +0800

    YoudaoTranslate()

--- translate/youdao/youdao_translate.py
+++ translate/youdao/youdao_translate.py
@@ -4,3 +4,44 @@
 # @Email: yhw_software@qq.com
 # @File: youdao_translate.py
 # @Software: PyCharm
+import hashlib
+import requests
+import time
+import random
+import json
+
+
+class YoudaoTranslate(object):
+    def __init__(self):
+        pass
+
+    def generate_ts_salt_sign(self, word):
+        """
+        生成 ts、salt、sign 参数
+        :return:
+        """
+
+    def request_data(self, url, data):
+        """
+        发送翻译请求
+        :param url:
+        :param words:
+        :return:
+        """
+
+    def parse_data(self, data):
+        """
+        解析数据
+        :param data:
+        :return:
+        """
+
+    def run(self):
+        """
+        启动爬虫
+        :return:
+        """
+
+
+if __name__ == '__main__':
+    YoudaoTranslate().run()
