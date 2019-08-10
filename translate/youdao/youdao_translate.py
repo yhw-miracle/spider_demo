0542f47b4edecfcf4dfbfd373eea52042a7fe61d
yhw-miracle <yhw_software@qq.com>
2019-08-10 22:25:04 +0800

    __init__()

--- translate/youdao/youdao_translate.py
+++ translate/youdao/youdao_translate.py
@@ -13,7 +13,30 @@ import json
 
 class YoudaoTranslate(object):
     def __init__(self):
-        pass
+        self.headers = {
+            "Cookie": "OUTFOX_SEARCH_USER_ID=-1473530863@10.168.8.64; JSESSIONID=aaar3zlxnbn4jfaTFw7Xw; OUTFOX_SEARCH_USER_ID_NCOO=1472877600.7083225; ___rl__test__cookies=1565437688138",
+            "Referer": "http://fanyi.youdao.com/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+        }
+
+        self.translate_url = "http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+        self.form_data = {
+            "i": None,
+            "from": "AUTO",
+            "to": "AUTO",
+            "smartresult": "dict",
+            "client": "fanyideskweb",
+            "salt": None,
+            "sign": None,
+            "ts": None,
+            "bv": "7e3150ecbdf9de52dc355751b074cf60",
+            "doctype": "json",
+            "version": "2.1",
+            "keyfrom": "fanyi.web",
+            "action": "FY_BY_REALTlME"
+        }
 
     def generate_ts_salt_sign(self, word):
         """
