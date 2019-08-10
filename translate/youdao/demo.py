a478e9739d7255ff04b700965918193e73ec60ab
yhw-miracle <yhw_software@qq.com>
2019-08-10 22:22:11 +0800

    hashlib

---
+++ translate/youdao/demo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time: 2019/8/10 22:21
+# @Author: yhw-miracle
+# @Email: yhw_software@qq.com
+# @File: demo.py
+# @Software: PyCharm
+import hashlib
+
+
+if __name__ == '__main__':
+    # 练习 hashlib 模块
+    md5 = hashlib.md5()
+    md5.update("7e3150ecbdf9de52dc355751b074cf60".encode("utf-8"))
+
+    print(md5.hexdigest())
+
+    # sha1 = hashlib.sha1()
+    md51 = hashlib.md5()
+    md51.update("7e3150ecbdf9de52dc355751b074cf60".encode("utf-8"))
+    print(md51.hexdigest())
+
+    print(md5.hexdigest() == md51.hexdigest())
---
+++ translate/youdao/youdao_translate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time: 2019/8/10 22:20
+# @Author: yhw-miracle
+# @Email: yhw_software@qq.com
+# @File: youdao_translate.py
+# @Software: PyCharm
