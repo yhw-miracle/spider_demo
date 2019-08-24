2b538fc9dbb913eab569ec1d6cf3dd0bae9251f7
yhw-miracle <yhw_software@qq.com>
2019-08-24 20:35:32 +0800

    初步确定爬取数据

--- JD_spider/JD_spider/items.py
+++ JD_spider/JD_spider/items.py
@@ -1,14 +1,55 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+# @Time: 2019/8/24 20:07
+# @Author: yhw-miracle
+# @Email: yhw_software@qq.com
+# @File: demo.py
+# @Software: PyCharm
+# use scrapy
 
 import scrapy
 
 
-class JdSpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class JdGoodsCategoryItem(scrapy.Item):
+    """
+    京东商品分类数据
+    """
+
+    # 一级分类
+    category1_name = scrapy.Field()
+    # 一级分类链接
+    category1_url = scrapy.Field()
+    # 二级分类
+    category2_name = scrapy.Field()
+    # 二级分类链接
+    category2_url = scrapy.Field()
+    # 三级分类
+    category3_name = scrapy.Field()
+    # 三级分类链接
+    category3_url = scrapy.Field()
+
+
+class JdGoodsItem(scrapy.Item):
+    """
+    京东商品数据
+    """
+
+    # 商品所属分类
+    category3 = scrapy.Field()
+    # 商品id
+    sku_id = scrapy.Field()
+    # 商品名称
+    name = scrapy.Field()
+    # 商品图片链接
+    image_url = scrapy.Field()
+    # 商品信息
+    info = scrapy.Field()
+    # 商品价格
+    price = scrapy.Field()
+    # 商品选项
+    option = scrapy.Field()
+    # 商品店铺
+    shop = scrapy.Field()
+    # 商品促销信息
+    advertisement = scrapy.Field()
+    # 商品评论量
+    comment = scrapy.Field()
